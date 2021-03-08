7ee608a3522d5514370949160fecfe3be11c2013
Karina <kstifurska@gmail.com>
2021-03-08 15:07:34 +0200

    Iespeja paslept paradit bildes

---
+++ Assets/Skripti/AtteliParadisana.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtteliParadisana : MonoBehaviour
+{
+    //Uzglabas musu bildes no ainas
+    public GameObject bins;
+    public GameObject lacitis;
+    public GameObject tante;
+    public GameObject masina;
+
+    public void binaAttelosana(bool vertiba)
+    {
+        bins.SetActive(vertiba);
+    }
+
+    public void lacisaAttelosana(bool vertiba)
+    {
+        lacitis.SetActive(vertiba);
+    }
+
+    public void tanteAttelosana(bool vertiba)
+    {
+        tante.SetActive(vertiba);
+    }
+
+    public void masinaAttelosana(bool vertiba)
+    {
+        masina.SetActive(vertiba);
+    }
+
+    public void paKreisiBins()
+    {
+        bins.transform.localScale = new Vector2(1, 1);
+    }
+
+    public void paLabiBins()
+    {
+        bins.transform.localScale = new Vector2(-1, 1);
+    }
+
+
+}
--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Pirmais Unity 2D projekts
 ### Veicamo darbu saraksts
 - [x] Nodrošināt projekta versionēšanu
 - [x] Ivdeivot testa logu, taksta ievades lauku un pogu 
-- [ ] Ievietot attēlus
-- [ ] Ievietot ček bokšus
-- [ ] Nodrošināt radio pogas darbību 
+- [x] Ievietot attēlus
+- [x] Ievietot ček bokšus
+- [x] Nodrošināt radio pogas darbību 
 - [ ] Izveidot izkrītoša saraksts poga
 - [ ] Nodrosināt attēla izmēra maiņu ar bīdjoslu
 - [ ] Nodrosināt attēla pārvietošanu peles palīdzību
